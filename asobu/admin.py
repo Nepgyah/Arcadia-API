caeb378b1445c816680b196d85b8bd476393b4da
Auston <nepgyah@gmail.com>
2026-03-13 18:47:28 -0500

    feat: add asobu admin settings

--- asobu/admin.py
+++ asobu/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from .models import (
+    Developer,
+    Platform,
+    Publisher,
+    Tag,
+    Game,
+    DLC,
+    GamePlatform,
+    GameRelation,
+    GameCharacter
+)
 
-# Register your models here.
+class GameCharacterInline(admin.TabularInline):
+    model = GameCharacter
+    extra = 1
+    autocomplete_fields = ['character']
+
+class GameRelationInline(admin.TabularInline):
+    model = GameRelation
+    extra = 1,
+    autocomplete_fields = ['from_game']
+
+class GamePlatformInline(admin.TabularInline):
+    model = GamePlatform
+    extra = 1
+    autocomplete_fields = ['platform']
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    inlines = [GameCharacterInline, GameRelation]
+    search_fields = ['title']
+    
+admin.site.register(Developer)
+admin.site.register(Platform)
+admin.site.register(Publisher)
+admin.site.register(Tag)
+admin.site.register(DLC)
+
