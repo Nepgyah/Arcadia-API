36bb50ac10b35d02b070ab1e37ea3e751e7e9c8d
Auston <nepgyah@gmail.com>
2026-03-17 16:49:35 -0500

    chore: create linter action

--- main/settings.py
+++ main/settings.py
@@ -10,9 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os
+from datetime import timedelta
 from pathlib import Path
 from dotenv import load_dotenv
-from datetime import timedelta
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
