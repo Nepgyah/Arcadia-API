1c817ea4b337cbb143a92536732074340cca010e
Auston <nepgyah@gmail.com>
2026-03-19 19:27:38 -0500

    feat: add custom form for franchise socials

--- base/admin.py
+++ base/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
+from .forms import FranciseForm
 from .models import (
     Franchise,
     Genre
 )
-admin.site.register(Franchise)
+
 admin.site.register(Genre)
+
+@admin.register(Franchise)
+class FranchiseAdmin(admin.ModelAdmin):
+    form = FranciseForm
--- talent/admin.py
+++ talent/admin.py
@@ -8,7 +8,8 @@ from .models import (
 @admin.register(Character)
 class CharacterAdmin(admin.ModelAdmin):
     search_fields = ['first_name', 'last_name']
-
+    autocomplete_fields = ['voice_actor']
+    
 @admin.register(VoiceActor)
 class VoiceActorAdmin(admin.ModelAdmin):
     search_fields = ['first_name', 'last_name']
