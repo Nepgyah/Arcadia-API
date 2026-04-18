2e1cf9af765c4bc92b7f746e333c8606db012f79
Auston Pawell <nepgyah@gmail.com>
2026-04-18 23:21:53 +0900

    feat: add asobu list entry model

--- asobu/models/list.py
+++ asobu/models/list.py
@@ -1,4 +1,38 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from game import Game
+from users.models import ArcadiaUser
 
 class AsobuListEntry(models.Model):
-    pass
+        
+    class StatusType(models.IntegerChoices):
+        PLAYING = 0, 'Playing'
+        COMPLETED = 1, 'Completed'
+        PLAN_TO = 2, 'Plan To Play'
+        ON_HOLD = 3, 'On Hold'
+        REPLAYING = 4, 'Replaying'
+    
+    user = models.ForeignKey(ArcadiaUser, on_delete=models.CASCADE, related_name='game_list')
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    status = models.IntegerField(choices=StatusType, default=StatusType.PLAYING)
+    score = models.SmallIntegerField(
+        blank=True, 
+        null=True,
+        validators=[MinValueValidator(1), MaxValueValidator(10)]
+    )
+    note = models.CharField(null=True, blank=True, max_length=256)
+    review = models.TextField(null=True, blank=True)
+    start_play_date = models.DateField(null=True, blank=True)
+    end_play_date = models.DateField(null=True, blank=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-updated_at']
+        indexes = [
+            models.Index(fields=['status']),
+        ]
+        unique_together = ['user', 'game']
+
+    def __str__(self):
+        return f'User: {self.user.username} - Game: {self.game.title} - Status: {self.get_status_display()}'
--- miru/models/list_entry.py
+++ miru/models/list_entry.py
@@ -23,7 +23,6 @@ class AnimeListEntry(models.Model):
     class Meta:
         ordering = ['-created_at']
         indexes = [
-            models.Index(fields=['anime']),
             models.Index(fields=['status']),
             models.Index(fields=["user", "status"]),
         ]
