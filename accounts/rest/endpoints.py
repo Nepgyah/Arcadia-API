26e19cbc742ad31ffa66c3639ff1b92eddf046b0
Auston <nepgyah@gmail.com>
2026-07-02 19:10:44 -0500

    feat: improve admin user login logic

--- accounts/rest/endpoints.py
+++ accounts/rest/endpoints.py
@@ -4,33 +4,26 @@ from accounts.service import AccountsService
 
 @api_view(['POST'])
 def adminLoginView(request):
-    try:
-        tokens = AccountsService.authentication.login_as_admin(
-            request.data.get('email', None),
-            request.data.get('password', None)
-        )
+    tokens = AccountsService.authentication.login_as_admin(
+        request.data.get('email', None),
+        request.data.get('password', None)
+    )
 
-        return Response(status=200, data={
-            'detail': "account_admin_login_success",
-            'message': "Login success",
-            'data': {
-                'access': {
-                    'value': tokens['access']['value'],
-                    'expiry': tokens['access']['expiry']
-                },
-                'refresh': {
-                    'value': tokens['refresh']['value'],
-                    'expiry': tokens['refresh']['expiry']
-                }
+    return Response(status=200, data={
+        'detail': "account_admin_login_success",
+        'message': "Login success",
+        'data': {
+            'access': {
+                'value': tokens['access']['value'],
+                'expiry': tokens['access']['expiry']
+            },
+            'refresh': {
+                'value': tokens['refresh']['value'],
+                'expiry': tokens['refresh']['expiry']
             }
-        })
-    except Exception as e:
-        return Response(
-            status=500,
-            data={
-                "message": "An unexpected error occured while logging in"
-            }
-        )
+        }
+    })
+
 
 @api_view(['POST'])
 def refreshTokenView(request):
